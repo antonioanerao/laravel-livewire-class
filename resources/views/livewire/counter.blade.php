8c585de4c37d06f4cafc8da093ffdc6323d8598e
Antonio Anerão <anerao.junior@gmail.com>
2021-05-28 22:52:08 -0500

    First commit; Installing livewire; Trying increment and decrement button

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button class="btn btn-success" wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    <button class="btn btn-success" wire:click="decrement">-</button>
+</div>
